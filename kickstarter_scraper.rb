afb600486a9af6589db71087a3c6d314153d5f2f
Megan Dacus <meganjustice0609@gmail.com>
2023-05-29 16:30:20 +1000

    Removed commented out section

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -23,9 +23,3 @@ end
 
 ap create_project_hash
 
-# projects: kickstarter.css("li.project.grid_4")
-# title: project.css("h2.bbcard_name strong a").text
-# image link: project.css("div.project-thumbnail a img").attribute("src").value
-# description: project.css("p.bbcard_blurb").text.strip
-# location: project.css("ul.project-meta span.location-name").text
-# percent_funded: project.css("ul.project-stats li.first.funded strong").text.gsub("%","").to_i
